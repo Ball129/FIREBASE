2c0880edb6df8f19ef540cd3a22ff71bdaa98f85
paparn <paparnball@gmail.com>
2020-09-15 22:47:57 +0700

    add storageService

---
+++ storageService.js
@@ -0,0 +1,22 @@
+class StorageService {
+    static async getImageUrl(storage, imageRef) {
+        return storage.child(imageRef).getDownloadURL().then(function (url) {
+            // `url` is the download URL for 'images/stars.jpg'
+
+            // This can be downloaded directly:
+            var xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob';
+            xhr.onload = function (event) {
+                var blob = xhr.response;
+            };
+            xhr.open('GET', url);
+            xhr.send();
+
+            return url;
+        }).catch(function (error) {
+            return ''
+        })
+    }
+}
+
+export default StorageService
